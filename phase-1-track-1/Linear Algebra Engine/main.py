f3fe8b71f6a09e6567eabbce8bc8537311effe9f
Deva92dev <buildwith92deva@gmail.com>
2026-07-10 21:19:06 +0530

    added vector and matrix classes

--- phase-1-track-1/Linear Algebra Engine/main.py
+++ phase-1-track-1/Linear Algebra Engine/main.py
@@ -1,1 +1,20 @@
-print("deva")
+class Vector:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Matrix:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
