1118b1ce03b66eef52e6449fdb40654cc68fdd43
Deva92dev <buildwith92deva@gmail.com>
2026-07-13 19:59:26 +0530

    added pagination in books API project

--- phase-1-track-2/example/app.py
+++ phase-1-track-2/example/app.py
@@ -8,9 +8,19 @@ app = FastAPI(title="My Books API")
 
 
 class Book(BaseModel):
+    id: int
     title: str
     author: str
+    price: int
     year_published: int
+
+
+class PaginatedBooks(BaseModel):
+    data: List[Book]
+    total: int
+    limit: int
+    offset: int
+    count: int
 
 
 #  In-memory database
@@ -29,30 +39,57 @@ async def create_one_book(book: Book):
 #     return books_database[skip : skip + limit]
 
 
-@app.get("/books/", status_code=status.HTTP_200_OK, response_model=List[Book])
+@app.get("/books/", status_code=status.HTTP_200_OK, response_model=PaginatedBooks)
 async def get_all_books(
-    author: Optional[str] = Query(None, description="Search By Author name"),
+    q: Optional[str] = Query(None, description="Search by book name", min_length=5),
+    author: Optional[str] = Query(
+        None, description="Search By Author name", min_length=5
+    ),
+    min_price: Optional[int] = Query(None),
+    max_price: Optional[int] = Query(None),
+    limit: int = Query(2, ge=1, le=5, description="Items per page"),
+    offset: int = Query(0, ge=0, description="Page Offset"),
 ):
     results = books_database
+
+    if q:
+        q_lower = q.lower()
+        results = [item for item in results if q_lower in item.title.lower()]
 
     if author:
         results = [item for item in results if item.author == author]
 
-    return results
+    if min_price is not None:
+        results = [item for item in results if item.price >= min_price]
+    if max_price is not None:
+        results = [item for item in results if item.price <= max_price]
+
+    paged_items = results[offset : offset + limit]
+
+    return {
+        "data": paged_items,
+        "total": len(results),
+        "limit": limit,
+        "offset": offset,
+        "count": len(paged_items),
+    }
 
 
-@app.get("/books/{title}", response_model=List[Book])
-async def get_single_book(title: str):
-    single_book_data = [item for item in books_database if item.title == title]
+@app.get("/books/{id}", response_model=List[Book])
+async def get_single_book(id: int):
+    single_book_data = [item for item in books_database if item.id == id]
     return single_book_data
 
 
-@app.patch("/books/{title}", response_model=Book)
-async def update_single_book(title: str, book_update: Book):
+# fix this
+@app.patch("/books/{id}", response_model=Book)
+async def update_single_book(id: int, book_update: Book):
     for i, item in enumerate(books_database):
-        if item.title == title:
+        if item.id == id:
             # Convert existing dict to Pydantic model
-            stored_book = Book(title="some", author="Author", year_published=2025)
+            stored_book = Book(
+                title="some", author="Author", year_published=2025, id=145, price=150
+            )
 
             # Get only the fields explicitly sent in the request
             update_data = book_update.model_dump(exclude_unset=True)
@@ -64,24 +101,24 @@ async def update_single_book(title: str, book_update: Book):
             books_database[i] = jsonable_encoder(updated_book)
             return updated_book
 
-    raise HTTPException(status_code=404, detail=f"Item '{title}' not found")
+    raise HTTPException(status_code=404, detail=f"Item '{id}' not found")
 
 
 # helper function to find book to delete
-def find_book(title: str):
+def find_book(id: int):
     for index, item in enumerate(books_database):
-        if item.title == title:
+        if item.id == id:
             return index
     return None
 
 
-@app.delete("/books/{title}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(title: str):
-    item = find_book(title)
+@app.delete("/books/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(id: int):
+    item = find_book(id)
 
     if item is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"{title} not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found"
         )
 
     books_database.pop(item)
---
+++ practice.py
@@ -0,0 +1,37 @@
+books = [
+    {
+        "id": 1,
+        "title": "Godan",
+        "author": "Premchand",
+        "price": 150,
+        "year_published": 1950,
+    },
+    {
+        "id": 2,
+        "title": "Namak",
+        "author": "Premchand",
+        "price": 170,
+        "year_published": 1952,
+    },
+    {
+        "id": 3,
+        "title": "Sipahi",
+        "author": "Premchand",
+        "price": 250,
+        "year_published": 1946,
+    },
+    {
+        "id": 4,
+        "title": "Geetanjali",
+        "author": "Tagore",
+        "price": 300,
+        "year_published": 1914,
+    },
+    {
+        "id": 5,
+        "title": "Anandmath",
+        "author": "Tagore",
+        "price": 400,
+        "year_published": 1932,
+    },
+]
