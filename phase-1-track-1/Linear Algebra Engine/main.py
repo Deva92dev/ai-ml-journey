4cc4482d1b46347459f3ac877c3c27e7d11b18d6
Devashish Rai <buildwith92deva@gmail.com>
2026-08-23 19:14:50 +0530

    Update linear algebra engine

---
+++ phase-1-track-1/Linear Algebra Engine/main.py
@@ -0,0 +1,184 @@
+import math
+
+# that is how you must ask questions to google, What → Why → Should → When → How
+
+
+class Vector:
+    #  (*args) to allow instantiation like Vector(1, 2, 3) or Vector(1, 2, 3, 4)
+    def __init__(self, *args):
+        if not args:
+            raise ValueError("Can not create an empty Vector")
+        self.components = args
+        if not all(isinstance(item, (float, int)) for item in self.components):
+            raise TypeError("All items must be integer or float")
+
+    def __iter__(self):
+        yield from self.components
+
+    def __len__(self):
+        return len(self.components)   
+
+    def __getitem__(self, key):
+        return self.components[key]
+
+    def __eq__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+
+        return self.components == other.components
+
+    # asterisk unpacks the list *
+    def __add__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("Both must be of same type")
+        if len(self) != len(other):
+            raise ValueError("Vectors must have the same dimension")
+
+        result = Vector(*[a + b for a, b in zip(self.components, other.components)])
+        return result
+
+    def __sub__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("Both must be of same type")
+        if len(self) != len(other):
+            raise ValueError("Vectors must have the same dimension")
+
+        result = Vector(*[a - b for a, b in zip(self.components, other.components)])
+        return result
+
+    def __mul__(self, scalar):
+        if not isinstance(scalar, (int, float)):
+            raise TypeError("Scalar must be int or float")
+
+        result = Vector(*[x * scalar for x in self.components])
+        return result
+
+    def __rmul__(self, scalar):
+        return self * scalar
+
+    def dot_product(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("Both must be of same type")
+        if len(self) != len(other):
+            raise ValueError("Vectors must have the same dimensions")
+
+        result = sum(a * b for a, b in zip(self.components, other.components))
+        return result
+
+    # calculates magnitudes, euclidean norm
+    def norm(self):
+        return math.sqrt(sum(c * c for c in self.components))
+
+    def __repr__(self):
+        return f"Vector({', '.join(map(str, self.components))})"
+
+
+v = Vector(12, 3, 45)
+w = Vector(15, 5, 65)
+result = v.__add__(w)
+print(result)
+
+
+class Matrix:
+    def __init__(self, *args):
+        if not args:
+            raise ValueError("Can not create an empty Matrix")
+        self.rows = args
+        for item in self.rows:
+            if not isinstance(item, tuple):
+                raise TypeError("Every item must be tuple")
+            if not all(isinstance(every, (int, float)) for every in item):
+                raise TypeError("Every item must be int or float")
+        if not all(len(row) == len(self.rows[0]) for row in self.rows):
+            raise ValueError("Every row must have the same length")
+
+    def __len__(self):
+        return len(self.rows)
+
+    def __getitem__(self, key):
+        return self.rows[key]
+
+    def __iter__(self):
+        yield from self.rows
+
+    def __eq__(self, other):
+        if not isinstance(other, Matrix):
+            return NotImplemented
+
+        return self.rows == other.rows
+
+    def __repr__(self):
+        return f"Matrix({', '.join(map(str, self.rows))})"
+
+    def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError("Both must be of same type")
+        if len(self.rows) != len(other.rows) and len(self.rows[0]) != len(
+            other.rows[0]
+        ):
+            raise ValueError("Both matrices must have the same dimension and order")
+
+        result = tuple(
+            tuple(a + b for a, b in zip(t1, t2))
+            for t1, t2 in zip(self.rows, other.rows)
+        )
+        return result
+
+    def __sub__(self, other):
+        if not isinstance(other, Matrix):
+            raise TypeError("Both must be of same type")
+        if len(self.rows) != len(other.rows) and len(self.rows[0]) != len(
+            other.rows[0]
+        ):
+            raise ValueError("Both matrices must have the same dimension and order")
+
+        result = tuple(
+            tuple(a - b for a, b in zip(t1, t2))
+            for t1, t2 in zip(self.rows, other.rows)
+        )
+        return result
+
+    def __mul__(self, scalar):
+        if not isinstance(scalar, (int, float)):
+            raise TypeError("Scalar must be int or float")
+
+        result = [[x * scalar for x in each] for each in self.rows]
+        answer = tuple(tuple(row) for row in result)
+        return answer
+
+    def transpose_matrix(self):
+        row = len(self.rows)
+        col = len(self.rows[0])
+        transposed = []
+
+        for j in range(col):
+            new = []
+            for i in range(row):
+                new.append(self.rows[i][j])
+            transposed.append(new)
+
+        return transposed
+
+    def vector_multiplication(self, vect):
+        if not isinstance(vect, Vector):
+            raise TypeError("Vect must be type of vector")
+        
+
+
+
+a = Matrix((10, 20, 30), (40, 50, 60), (70, 80, 90))
+m = Matrix((1, 2, 7), (3, 4, 8), (5, 6, 9))
+n = Matrix((1, 2, 7), (3, 4, 8), (5, 6, 9))
+v = m.__add__(n)
+w = m.__sub__(n)
+x = m.__mul__(5)
+print(x)
+print(v)
+print(w)
+print(m)
+print(m[2][0])
+for item in m.rows:
+    print(item[0], item[1], item[2])
+
+new = a.transpose_matrix()
+print(new)
---
+++ phase-1-track-1/Linear Algebra Engine/requirements.md
@@ -0,0 +1,79 @@
+# Vector Operations
+
+- In programming, a vector typically refers to a dynamic array, a data structure that stores a sequence of elements of the same type and automatically resizes itself as elements are added or removed
+
+- In mathematics and machine learning, a vector is also an ordered list of scalars
+  (e.g., <x, y, z>) used to represent data points, features, or physical quantities with both magnitude and direction.
+
+- Store values as tuple
+- Tuples have a slightly smaller memory footprint than lists because they don't need extra space for resizing.
+- in case of invalid vector, raise error
+- every vector must have repr, addition, multiplication, truediv, rmul, norm, etc
+
+- use len-dunder method to get the length of Vector class
+- If there is a empty vector, it must result in error or undefined behavior.
+
+- the asterisk \* is the unpacking operator
+
+- Utility operations
+- Where A , B are vectors
+- Addition: Dimensions must match
+- Subtraction: Dimensions must match
+- Scalar Product = n . A
+- Cross Product = A x B = |A| |B| Sinθ : Dimensions must match
+- Dot Product(Scalar Product) = A ⋅ B = |A| |B| . cosθ : Dimensions must match
+- If dot product is zero(what happens)
+
+- dot product produces a scalar value representing the alignment of two vectors. It Measures projection and work.
+- the cross product produces a vector perpendicular to the plane of the original two. It Measures perpendicularity and area
+
+# Matrix Operations
+
+- In software, a matrix is a two-dimensional data structure (often called a 2D array) that organizes values into rows and columns. It serves as a compact, efficient container for structured data, such as grid-based images, game boards, or mathematical models.
+  - rows of tuple
+
+- Empty, Null Matrices
+  - an empty matrix (a tuple of tuple with no rows) is detected by checking if the outer list is empty using len(matrix) == 0 or the boolean evaluation "not matrix"
+  - a null matrix (a matrix where all elements are zero) in pure Python without external libraries, you can iterate through the nested list structure to verify that every element equals zero.
+
+- Invalid Matrix
+  - An invalid matrix in Python (represented as a tuple of tuples) is detected by ensuring two conditions: every element is a list (checking if the outer structure is a matrix) and all inner lists have the same length (ensuring rectangular consistency)
+
+  - Type Check: Use isinstance(matrix, tuple) to confirm the input is a list.
+
+  - Row Type Check: Verify every element in the outer list is itself a list using isinstance(row, list).
+  - Dimension Consistency: Ensure all rows have the same number of elements (e.g., comparing len(row) to len(matrix[0]))
+
+  - Matrix multiplication is impossible when the number of columns in the first matrix does not equal the number of rows in the second matrix
+
+- Accept integers as well as floating point integers(make sure to limit it to 2 places after decimal)
+
+- A matrix is not invertible if it has non-square dimensions, its determinant is 0, zero eigenvalues and zero rows and column.
+
+- Utility operations
+- Addition
+- Subtraction  
+- Multiplication
+- Transpose
+- (Gaussian elimination method)
+  - Determinant
+  - Identity matrix
+  - Inverse
+  - Rank
+- Eigenvalues and Eigenvectors
+
+# Extensibility
+
+- For each future operation, do this:
+  - A normal case.
+  - A boundary case.
+  - An invalid case.
+
+- Matrix multiplication, Vector and Scalar Operations, Transpose operations will come very handy in Gradient descent and Neural Network projects
+
+- In vector * matrix, matrix-vector multiplication is a linear transformation that maps a vector from one coordinate space to another, effectively rotating, scaling, or shearing the input vector.
+
+- How much of the input vector x points in the direction of the i -th row of the matrix?
+- number of rows of vector must be equal to number of columns of matrix. while the number of rows determines the dimension of the resulting vector
+
+- 
